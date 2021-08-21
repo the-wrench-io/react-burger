39967ca36e459e7191af2235a23d6a714c967a50
Olev Mutso <olev.mutso@resys.io>
2021-08-21 12:50:43 +0300

    export

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,16 +1,20 @@
 
 import { default as themeAs } from '../themes';
 import { default as intlAs } from '../intl';
-
+import { API as Context, Integration } from './context';
 
 
 declare namespace Layout {
-  
+  export { Context };
 }
 
 namespace Layout {
   export const themes = themeAs;
   export const intl = intlAs;
+  export const Provider = Integration.Provider;
+  export const useContext = Integration.useContext;
 };
+
+
 
 export default Layout;
