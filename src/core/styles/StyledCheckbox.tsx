56b4f5d9fc720d66310a2951a86ddd2146941656
Jocelyn Mutso <47987958+jocelynmutso@users.noreply.github.com>
2022-10-22 09:36:21 +0300

    Update StyledCheckbox.tsx

--- src/core/styles/StyledCheckbox.tsx
+++ src/core/styles/StyledCheckbox.tsx
@@ -15,10 +15,14 @@ const StyledCheckboxRoot = styled(Checkbox)<CheckboxProps>(({ theme }) => ({
 
 const StyledCheckbox: React.FC<{
   checked: boolean;
-  onChange: (newValue: boolean) => void;
+  onChange?: (newValue: boolean) => void;
   sx?: SxProps<Theme>;
 }> = (props) => {
-  return (<StyledCheckboxRoot value={props.checked} sx={props.sx} onChange={({target}) => props.onChange(target.checked)} />)
+  return (<StyledCheckboxRoot value={props.checked} sx={props.sx} onChange={({target}) => {
+    if(props.onChange) {
+      props.onChange(target.checked);
+    }
+  }} />)
 }
 
 export { StyledCheckbox }
